ac425330867199e7a158c7882951102d76bbeaa5
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-04-04 11:51:45 +0530

    Add Bernoulli and Gaussian Bandits

--- jigglypuffRL/classicalRL/karmedBandit.py
+++ jigglypuffRL/classicalRL/karmedBandit.py
@@ -6,7 +6,24 @@ class Bandit(object):
     def __init__(self, bandits=1, arms=1):
         self.nbandits = bandits
         self.narms = arms
+
+    def learn(self, n_timesteps=None):
+        raise NotImplementedError    
+
+
+class BernoulliBandits(Bandit):
+    def __init__(self, bandits=1, arms=1):
+        super(BernoulliBandits, self).__init__(bandits, arms)
+        self.bandits = np.random.random((self.nbandits, self.narms))
+
+    def learn(self, n_timesteps=None):
+        raise NotImplementedError
+
+
+class GaussianBandits(Bandit):
+    def __init(self, bandits=1, arms=1):
+        super(GaussianBandits, self).__init__(bandits, arms)
         self.bandits = np.random.normal((self.nbandits, self.narms))
 
-    def learn(n_timesteps=None):
-        raise NotImplementedError    
+    def learn(self, n_timesteps=None):
+        raise NotImplementedError
