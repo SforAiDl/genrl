982128abb50873df5bec8bf6e403dbf58de86aca
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-04-07 15:49:40 +0530

    Correct load_param to load_params

--- jigglypuffRL/common/__init__.py
+++ jigglypuffRL/common/__init__.py
@@ -2,4 +2,4 @@ from jigglypuffRL.common.policies import BasePolicy, MlpPolicy, get_policy_from_
 from jigglypuffRL.common.values import MlpValue, get_value_from_name
 from jigglypuffRL.common.actor_critic import ActorCritic, MlpActor, MlpCritic
 from jigglypuffRL.common.buffers import ReplayBuffer
-from jigglypuffRL.common.utils import evaluate, save_params, load_param
+from jigglypuffRL.common.utils import evaluate, save_params, load_params
