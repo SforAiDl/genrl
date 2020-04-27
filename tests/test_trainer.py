f39cd79d04a742299d91e6c17dc740804d1f4040
Sharad Chitlangia <sharadchitlangia24sc@gmail.com>
2020-04-27 05:58:38 +0530

    Update test_trainer.py

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -14,6 +14,6 @@ def test_off_policy_trainer():
     logger = Logger()
     env = gym.make("Pendulum-v0")
     algo = TD3("mlp", env)
-    trainer = OnPolicyTrainer(algo, env, logger)
+    trainer = OffPolicyTrainer(algo, env, logger)
     assert trainer.off_policy == True
     trainer.train()
