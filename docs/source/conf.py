e46ca737be7914f59a2a3f28a7a161bb9b72e329
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-05-03 18:13:52 +0530

    Add autogeneration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../jigglypuffRL/'))
 
 
 # -- Project information -----------------------------------------------------
@@ -32,6 +32,7 @@ release = '0.2'
 # ones.
 extensions = [
     'sphinx.ext.githubpages',
+    'sphinx.ext.autodoc', 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
