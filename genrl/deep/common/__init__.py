bab12285e892948895e4d08973616205f5cea81b
Zeus3101 <12513634+Zeus3101@users.noreply.github.com>
2020-05-04 14:06:01 +0530

    Update __init__.py

--- genrl/deep/common/__init__.py
+++ genrl/deep/common/__init__.py
@@ -3,32 +3,33 @@ from genrl.deep.common.values import MlpValue, get_value_from_name  # noqa
 from genrl.deep.common.actor_critic import (  # noqa
     MlpActorCritic,
     get_actor_critic_from_name,
-)  # noqa
-from genrl.deep.common.base import (
+)
+from genrl.deep.common.base import (  # noqa
     BasePolicy,
     BaseValue,
     BaseActorCritic,
-)  # noqa
+)
 from genrl.deep.common.buffers import ReplayBuffer, PrioritizedBuffer  # noqa
 from genrl.deep.common.utils import (  # noqa
     mlp,
+    cnn,
     get_model,
     evaluate,
     save_params,
     load_params,
     set_seeds,
 )
-from genrl.deep.common.VecEnv import (
+from genrl.deep.common.VecEnv import (  # noqa
     SerialVecEnv,
     SubProcessVecEnv,
     venv,
-)  # noqa
+)
 from genrl.deep.common.noise import (  # noqa
     NormalActionNoise,
     OrnsteinUhlenbeckActionNoise,
 )
-from genrl.deep.common.logger import Logger
-from genrl.deep.common.trainer import (
+from genrl.deep.common.logger import Logger  # noqa
+from genrl.deep.common.trainer import (  # noqa
     OffPolicyTrainer,
     OnPolicyTrainer,
 )
