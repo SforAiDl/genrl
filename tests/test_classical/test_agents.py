192e4fd21e4b39302e1b9313541a4c850148b81b
Ajay Subramanian <ajaysubramanian110@gmail.com>
2020-05-06 09:50:52 +0530

    add sarsa test

--- tests/test_classical/test_agents.py
+++ tests/test_classical/test_agents.py
@@ -14,6 +14,23 @@ class TestAgents:
         agent.update((3, 1, 3.1, 4))
         answer = np.zeros((16,4))
         answer[3,1] += 0.031000000000000003
-        assert np.all(agent.Q == answer)
+        assert np.all(agent.Q == pytest.approx(answer))
 
         assert agent.get_action(3,False) == 1
+
+    def test_sarsa(self):
+        env = gym.make('FrozenLake-v0')
+        agent = SARSA(env)
+
+        assert np.any(agent.Q) == False
+        assert np.any(agent.e) == False
+
+        agent.update((3, 1, 3.1, 4))
+        answer_Q = np.zeros((16,4))
+        answer_e = np.zeros((16,4))
+        answer_Q[3,1] = 0.21378364
+        answer_e[3,1] = 4.42416527e-05
+        
+        assert np.all(agent.Q == pytest.approx(answer_Q))
+        assert np.all(agent.e == pytest.approx(answer_e))
+        
