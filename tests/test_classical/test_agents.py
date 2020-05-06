7710ae3d47f1d8dae9479dbeb54e9397579546e0
Ajay Subramanian <ajaysubramanian110@gmail.com>
2020-05-06 09:54:38 +0530

    reformat changed files

--- tests/test_classical/test_agents.py
+++ tests/test_classical/test_agents.py
@@ -4,33 +4,33 @@ import gym
 
 from genrl import QLearning, SARSA
 
+
 class TestAgents:
     def test_qlearning(self):
-        env = gym.make('FrozenLake-v0')
+        env = gym.make("FrozenLake-v0")
         agent = QLearning(env)
 
         assert np.any(agent.Q) == False
 
         agent.update((3, 1, 3.1, 4))
-        answer = np.zeros((16,4))
-        answer[3,1] += 0.031000000000000003
+        answer = np.zeros((16, 4))
+        answer[3, 1] += 0.031000000000000003
         assert np.all(agent.Q == pytest.approx(answer))
 
-        assert agent.get_action(3,False) == 1
+        assert agent.get_action(3, False) == 1
 
     def test_sarsa(self):
-        env = gym.make('FrozenLake-v0')
+        env = gym.make("FrozenLake-v0")
         agent = SARSA(env)
 
         assert np.any(agent.Q) == False
         assert np.any(agent.e) == False
 
         agent.update((3, 1, 3.1, 4))
-        answer_Q = np.zeros((16,4))
-        answer_e = np.zeros((16,4))
-        answer_Q[3,1] = 0.21378364
-        answer_e[3,1] = 4.42416527e-05
-        
+        answer_Q = np.zeros((16, 4))
+        answer_e = np.zeros((16, 4))
+        answer_Q[3, 1] = 0.21378364
+        answer_e[3, 1] = 4.42416527e-05
+
         assert np.all(agent.Q == pytest.approx(answer_Q))
         assert np.all(agent.e == pytest.approx(answer_e))
-        
--- tests/test_classical/test_common/test_models.py
+++ tests/test_classical/test_common/test_models.py
@@ -7,17 +7,17 @@ from genrl.classical import TabularModel
 
 class TestModels:
     def test_tabular_model(self):
-        model = TabularModel(4,2)
+        model = TabularModel(4, 2)
 
-        assert model.s_model.shape == (4,2)
-        assert model.r_model.shape == (4,2)
+        assert model.s_model.shape == (4, 2)
+        assert model.r_model.shape == (4, 2)
         assert model.is_empty() == True
 
-        model.add(3,1,3.1,1)
+        model.add(3, 1, 3.1, 1)
         assert model.is_empty() == False
 
-        assert model.s_model[3,1] == 1
-        assert model.r_model[3,1] == 3.1
+        assert model.s_model[3, 1] == 1
+        assert model.r_model[3, 1] == 3.1
 
-        assert model.sample() == (3,1)
-        assert model.step(3,1) == (3.1,1)
+        assert model.sample() == (3, 1)
+        assert model.step(3, 1) == (3.1, 1)
