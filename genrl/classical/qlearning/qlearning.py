cb930d0c6a3613063443843a7376f0f6df06bdff
Zeus3101 <12513634+Zeus3101@users.noreply.github.com>
2020-05-10 17:27:01 +0530

    documented and reformatted sarsa.py and qlearning.py

--- genrl/classical/qlearning/qlearning.py
+++ genrl/classical/qlearning/qlearning.py
@@ -5,29 +5,52 @@ class QLearning:
     """
     Q-Learning Algorithm
     Paper: https://link.springer.com/article/10.1007/BF00992698
-    :param env: (gym environment) standard gym environment to train on
-    :param epsilon: (float) exploration coefficient
-    :param gamma: (float) discount factor
-    :param lr: (float) learning rate
-    """
 
+    :param env: Standard gym environment to train on
+    :param epsilon: Exploration coefficient
+    :param gamma: Discount factor
+    :param lr: Learning rate
+    :type env: Gym Environment
+    :type epsilon: float
+    :type gamma: float
+    :type lr: float
+    """
     def __init__(self, env, epsilon=0.9, gamma=0.95, lr=0.01):
         self.env = env
         self.epsilon = epsilon
         self.gamma = gamma
         self.lr = lr
 
-        self.Q = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.Q = np.zeros((
+            self.env.observation_space.n, self.env.action_space.n
+        ))
 
-    def get_action(self, s, explore=True):
-        if explore == True:
+    def get_action(self, state, explore=True):
+        """
+        Gives action which would give the highest Q-value
+
+        :param state: Input state for finding Q-values
+        :param explore: True if agent is exploring, else False
+        :type state: Numpy array
+        :type explore: bool
+        :returns: Action with the highest Q-value for the given state
+        """
+        if explore is True:
             if np.random.uniform() > self.epsilon:
                 return self.env.action_space.sample()
-        return np.argmax(self.Q[s, :])
+        return np.argmax(self.Q[state, :])
 
     def update(self, transition):
-        s, a, r, s_ = transition
+        """
+        Updates Q-values given next transition tuple
 
-        self.Q[s, a] += self.lr * (
-            r + self.gamma * np.max(self.Q[s_, :]) - self.Q[s, a]
+        :param transition: Tuple of state, action, reward and next state
+        :type transition: tuple
+        """
+        state, action, reward, next_state = transition
+
+        self.Q[state, action] += self.lr * (
+            reward
+            + self.gamma * np.max(self.Q[next_state, :])
+            - self.Q[state, action]
         )
--- genrl/classical/sarsa/sarsa.py
+++ genrl/classical/sarsa/sarsa.py
@@ -5,13 +5,18 @@ class SARSA:
     """
     SARSA Algorithm
     Paper: http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.17.2539&rep=rep1&type=pdf
-    :param env: (gym environment) standard gym environment to train on
-    :param epsilon: (float) exploration coefficient
-    :param lmbda: (float) eligibility trace coefficient
-    :param gamma: (float) discount factor
-    :param lr: (float) learning rate
-    """
 
+    :param env: Standard gym environment to train on
+    :param epsilon: Exploration coefficient
+    :param lmbda: Eligibility trace coefficient
+    :param gamma: Discount factor
+    :param lr: Learning rate
+    :type env: Gym Environment
+    :type epsilon: float
+    :type lmbda: float
+    :type gamma: float
+    :type lr: float
+    """
     def __init__(self, env, epsilon=0.9, lmbda=0.9, gamma=0.95, lr=0.01):
         self.env = env
         self.epsilon = epsilon
@@ -19,24 +24,51 @@ class SARSA:
         self.gamma = gamma
         self.lr = lr
 
-        self.Q = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.Q = np.zeros((
+            self.env.observation_space.n, self.env.action_space.n
+        ))
 
-        self.e = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.e = np.zeros((
+            self.env.observation_space.n, self.env.action_space.n
+        ))
 
-    def get_action(self, s, explore=True):
-        if explore == True:
+    def get_action(self, state, explore=True):
+        """
+        Gives action which would give the highest Q-value
+
+        :param state: Input state for finding Q-values
+        :param explore: True if agent is exploring, else False
+        :type state: Numpy array
+        :type explore: bool
+        :returns: Action with the highest Q-value for the given state
+        """
+        if explore is True:
             if np.random.uniform() > self.epsilon:
                 return self.env.action_space.sample()
-        return np.argmax(self.Q[s, :])
+        return np.argmax(self.Q[state, :])
 
     def update(self, transition):
-        s, a, r, s_ = transition
+        """
+        Updates Q-values given next transition tuple
 
-        a_ = self.get_action(s_)
-        delta = r + self.gamma * self.Q[s_, a_] - self.Q[s, a]
-        self.e[s, a] += 1
+        :param transition: Tuple of state, action, reward and next state
+        :type transition: tuple
+        """
+        state, action, reward, next_state = transition
+
+        next_action = self.get_action(next_state)
+        delta = (
+            reward
+            + self.gamma * self.Q[next_state, next_action]
+            - self.Q[state, action]
+        )
+        self.e[state, action] += 1
 
         for si in range(self.env.observation_space.n):
             for ai in range(self.env.action_space.n):
-                self.Q[s, a] += self.lr * delta * self.e[s, a]
-                self.e[s, a] = self.gamma * self.lmbda * self.e[s, a]
+                self.Q[state, action] += (
+                    self.lr * delta * self.e[state, action]
+                )
+                self.e[state, action] = (
+                    self.gamma * self.lmbda * self.e[state, action]
+                )
