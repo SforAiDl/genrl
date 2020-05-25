bdaf625743efdba607b82d373649313ddb21a1d0
Het-Shah <divhet163@gmail.com>
2020-05-25 18:21:41 +0530

    Add noise, policies, trainer

--- genrl/deep/common/policies.py
+++ genrl/deep/common/policies.py
@@ -1,5 +1,6 @@
 from .base import BasePolicy
 from .utils import mlp
+from typing import Tuple
 
 
 class MlpPolicy(BasePolicy):
@@ -16,9 +17,13 @@ class MlpPolicy(BasePolicy):
     :type discrete: bool
     """
 
-    def __init__(
-        self, state_dim, action_dim, hidden=(32, 32), disc=True, *args, **kwargs
-    ):
+    def __init__(self, 
+                 state_dim: int, 
+                 action_dim: int, 
+                 hidden: Tuple=(32, 32), 
+                 disc: bool=True, 
+                 *args, 
+                 **kwargs):
         super(MlpPolicy, self).__init__(state_dim, action_dim, hidden, disc, **kwargs)
 
         self.state_dim = state_dim
@@ -30,7 +35,7 @@ class MlpPolicy(BasePolicy):
 policy_registry = {"mlp": MlpPolicy}
 
 
-def get_policy_from_name(name_):
+def get_policy_from_name(name_:str):
     """
     Returns policy given the name of the policy
 
