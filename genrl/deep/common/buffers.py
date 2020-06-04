2901dc89475689247a8ebc3a3a18e36ac70a968e
Gaurav Iyer <48847874+infinitemugen@users.noreply.github.com>
2020-06-04 19:09:28 +0530

    update for HER buffer

---
+++ genrl/deep/common/buffers.py
@@ -0,0 +1,170 @@
+import torch
+from collections import deque
+import random
+import numpy as np
+from typing import Tuple
+
+
+class ReplayBuffer:
+    """
+    Implements the basic Experience Replay Mechanism
+
+    :param capacity: Size of the replay buffer
+    :type capacity: int
+    """
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, x: Tuple) -> None:
+        """
+        Adds new experience to buffer
+
+        :param x: Tuple containing state, action, reward, next_state and done
+        :type x: tuple
+        :returns: None
+        """
+        self.memory.append(x)
+
+    def sample(
+        self, batch_size: int
+    ) -> (Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]):
+        """
+        Returns randomly sampled experiences from replay memory
+
+        :param batch_size: Number of samples per batch
+        :type batch_size: int
+        :returns: Tuple composing of `state`, `action`, `reward`, \
+`next_state` and `done`
+        """
+        batch = random.sample(self.memory, batch_size)
+        state, action, reward, next_state, done = map(np.stack, zip(*batch))
+        return (
+            torch.as_tensor(v, dtype=torch.float32)
+            for v in [state, action, reward, next_state, done]
+        )
+
+    def get_len(self) -> int:
+        """
+        Gives number of experiences in buffer currently
+
+        :returns: Length of replay memory
+        """
+        return len(self.memory)
+
+
+class PrioritizedBuffer:
+    """
+    Implements the Prioritized Experience Replay Mechanism
+
+    :param capacity: Size of the replay buffer
+    :param alpha: Level of prioritization
+    :type capacity: int
+    :type alpha: int
+    """
+
+    def __init__(self, capacity: int, alpha: float = 0.6):
+        self.alpha = alpha
+        self.capacity = capacity
+        self.buffer = deque([], maxlen=capacity)
+        self.priorities = deque([], maxlen=capacity)
+
+    def push(self, x: Tuple) -> None:
+        """
+        Adds new experience to buffer
+
+        :param x: Tuple containing `state`, `action`, `reward`, \
+`next_state` and `done`
+        :type x: tuple
+        :returns: None
+        """
+        max_priority = max(self.priorities) if self.buffer else 1.0
+        self.buffer.append(x)
+        self.priorities.append(max_priority)
+
+    def sample(
+        self, batch_size: int, beta: float = 0.4
+    ) -> (
+        Tuple[
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+        ]
+    ):
+        """
+        Returns randomly sampled memories from replay memory along with their \
+respective indices and weights
+
+        :param batch_size: Number of samples per batch
+        :param beta: Bias exponent used to correct \
+Importance Sampling (IS) weights
+        :type batch_size: int
+        :type beta: float
+        :returns: Tuple containing `states`, `actions`, `next_states`, \
+`rewards`, `dones`, `indices` and `weights`
+        """
+        total = len(self.buffer)
+
+        priorities = np.asarray(self.priorities)
+
+        probabilities = priorities ** self.alpha
+        probabilities /= probabilities.sum()
+
+        indices = np.random.choice(total, batch_size, p=probabilities)
+
+        weights = (total * probabilities[indices]) ** (-beta)
+        weights /= weights.max()
+        weights = np.asarray(weights, dtype=np.float32)
+
+        samples = np.asarray(self.buffer, dtype=deque)[indices]
+        (states, actions, rewards, next_states, dones) = map(np.stack, zip(*samples))
+
+        return (
+            torch.as_tensor(v, dtype=torch.float32)
+            for v in [states, actions, rewards, next_states, dones, indices, weights]
+        )
+
+    def update_priorities(self, batch_indices: Tuple, batch_priorities: Tuple) -> None:
+        """
+        Updates list of priorities with new order of priorities
+
+        :param batch_indices: List of indices of batch
+        :param batch_priorities: List of priorities of the batch at the \
+specific indices
+        :type batch_indices: list or tuple
+        :type batch_priorities: list or tuple
+        """
+        for idx, priority in zip(batch_indices, batch_priorities):
+            self.priorities[int(idx)] = priority
+
+    def get_len(self) -> int:
+        """
+        Gives number of experiences in buffer currently
+
+        :returns: Length of replay memory
+        """
+        return len(self.buffer)
+
+class HindsightMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.index = 0
+
+    def push(self, experience):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.index] = experience
+        self.index = (self.index + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
