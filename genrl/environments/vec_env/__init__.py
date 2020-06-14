3237b85ff5c0428f554675f220017f66609b9d2b
Zeus3101 <12513634+Zeus3101@users.noreply.github.com>
2020-06-14 08:20:49 +0530

    finished vecmonitor

--- genrl/environments/vec_env/__init__.py
+++ genrl/environments/vec_env/__init__.py
@@ -1,3 +1,4 @@
 from genrl.environments.vec_env.utils import RunningMeanStd  # noqa
+from genrl.environments.vec_env.vec_monitor import VecMonitor  # noqa
 from genrl.environments.vec_env.vec_normalize import VecNormalize  # noqa
 from genrl.environments.vec_env.vector_envs import VecEnv  # noqa
--- genrl/environments/vec_env/vec_monitor.py
+++ genrl/environments/vec_env/vec_monitor.py
@@ -1,11 +1,25 @@
 import time
 from collections import deque
+from typing import Tuple
 
-from .vec_wrappers import VecEnvWrapper
+import numpy as np
+
+from .vec_wrappers import VecEnv, VecEnvWrapper
 
 
 class VecMonitor(VecEnvWrapper):
-    def __init__(self, venv, history_length=0, info_keys=()):
+    """
+    Monitor class for VecEnvs. Saves important variables into the info dictionary
+
+    :param venv: Vectorized Environment
+    :param history_length: Length of history for episode rewards and episode lengths
+    :param info_keys: Important variables to save
+    :type venv: object
+    :type history_length: int
+    :type info_keys: tuple or list
+    """
+
+    def __init__(self, venv: VecEnv, history_length: int = 0, info_keys: Tuple = ()):
         super(VecMonitor, self).__init__(venv)
 
         self.len = history_length
@@ -21,13 +35,26 @@ class VecMonitor(VecEnvWrapper):
             self.returns_history = deque([], maxlen=self.len)
             self.lens_history = deque([], maxlen=self.len)
 
-    def reset(self):
+    def reset(self) -> np.ndarray:
+        """
+        Resets Vectorized Environment
+
+        :returns: Initial observations
+        :rtype: Numpy Array
+        """
         observation = self.venv.reset()
         self.episode_returns = np.zeros(self.n_envs, dtype=np.float32)
         self.episode_lens = np.zeros(self.n_envs, dtype=int)
         return observation
 
-    def step(self, actions):
+    def step(self, actions: np.ndarray) -> Tuple:
+        """
+        Steps through all the environments and normalizes the observations and rewards (if enabled)
+
+        :param actions: Actions to be taken for the Vectorized Environment
+        :type actions: Numpy Array
+        :returns: States, rewards, dones, infos
+        """
         observations, rewards, dones, infos = self.venv.step(actions)
 
         self.episode_returns += rewards
