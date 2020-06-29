3e2ca390cbc1a6999d07e458f6a0209c042e0b31
Ajay Subramanian <ajaysubramanian110@gmail.com>
2020-06-30 00:33:23 +0530

    reformat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'GenRL'
-copyright = '2020, Society for Artificial Intelligence and Deep Learning (SAiDL)'
-author = 'Society for Artificial Intelligence and Deep Learning (SAiDL)'
+project = "GenRL"
+copyright = "2020, Society for Artificial Intelligence and Deep Learning (SAiDL)"
+author = "Society for Artificial Intelligence and Deep Learning (SAiDL)"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,10 +30,10 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.napoleon']
+extensions = ["sphinxcontrib.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,9 +46,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
