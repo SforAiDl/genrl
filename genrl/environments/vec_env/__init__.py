59ea8d61c2cb3b26b2bd942eb3f451272957e526
Sampreet <sampreetarthi@gmail.com>
2020-07-04 16:57:49 +0530

    sort travis build

--- genrl/environments/vec_env/__init__.py
+++ genrl/environments/vec_env/__init__.py
@@ -1,6 +1,6 @@
-from genrl.environments.vec_env.utils import RunningMeanStd  # noqa
 from genrl.environments.vec_env.monitor import VecMonitor  # noqa
 from genrl.environments.vec_env.normalize import VecNormalize  # noqa
+from genrl.environments.vec_env.utils import RunningMeanStd  # noqa
 from genrl.environments.vec_env.vector_envs import (  # noqa
     SerialVecEnv,
     SubProcessVecEnv,
