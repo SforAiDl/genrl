ff76e2354f340b74d10475e3a94cb8bcfae04428
Atharv Sonwane <atharvs.twm@gmail.com>
2020-07-07 14:50:41 +0530

    Seperated test for deep cbs

--- tests/test_classical/test_bandits.py
+++ tests/test_classical/test_bandits.py
@@ -3,18 +3,12 @@ from genrl import (
     BayesianUCBPolicy,
     BernoulliBandit,
     BernoulliCB,
-    CovertypeDataBandit,
     EpsGreedyCBPolicy,
     EpsGreedyPolicy,
     GaussianBandit,
     GaussianCB,
     GradientCBPolicy,
     GradientPolicy,
-    LinearPosteriorAgent,
-    MushroomDataBandit,
-    NeuralGreedyAgent,
-    NeuralLinearPosteriorAgent,
-    StatlogDataBandit,
     ThompsonSamplingCBPolicy,
     ThompsonSamplingPolicy,
     UCBCBPolicy,
@@ -81,37 +75,4 @@ class TestBandit:
     def test_thompson_bernoulli_cb(self) -> None:
         bandit = BernoulliCB(bandits=10, arms=10)
         policy = ThompsonSamplingCBPolicy(bandit)
-        policy.learn(10)
-
-    def test_covertype_data_bandit(self) -> None:
-        bandit = CovertypeDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_mushroom_data_bandit(self) -> None:
-        bandit = MushroomDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_statlog_data_bandit(self) -> None:
-        bandit = StatlogDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_linear_posterior_agent(self) -> None:
-        bandit = CovertypeDataBandit(download=True)
-        policy = LinearPosteriorAgent(bandit)
-        policy.learn(10)
-
-    def test_neural_linear_posterior_agent(self) -> None:
-        bandit = CovertypeDataBandit(download=True)
-        policy = NeuralLinearPosteriorAgent(bandit)
-        policy.learn(10)
-
-    def test_neural_greedy_agent(self) -> None:
-        bandit = CovertypeDataBandit(download=True)
-        policy = NeuralGreedyAgent(bandit)
         policy.learn(10)
