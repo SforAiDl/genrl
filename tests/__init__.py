d1295de8f3f7ebf8cb86fc52a06097762e388dc8
Sampreet <sampreetarthi@gmail.com>
2020-07-16 20:18:48 +0530

    isort errors

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,3 @@
 from tests.test_classical import TestAgents, TestBandit, TestModels, TestTrainer  # noqa
-from tests.test_deep import (  # noqa
-    TestUtils,
-    test_loggers,
-    test_off_policy_trainer,
-    test_on_policy_trainer,
-)
+from tests.test_deep import test_loggers  # noqa
+from tests.test_deep import TestUtils, test_off_policy_trainer, test_on_policy_trainer
