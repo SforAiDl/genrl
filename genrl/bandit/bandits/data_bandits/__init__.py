ab7cfd42d10eba4c9e576e7ba12fcc87f75f4dd4
Atharv Sonwane <atharvs.twm@gmail.com>
2020-07-27 22:49:50 +0530

    fixed isort issue

--- genrl/bandit/bandits/data_bandits/__init__.py
+++ genrl/bandit/bandits/data_bandits/__init__.py
@@ -1,9 +1,7 @@
-from genrl.bandit.bandits.data_bandits.adult_bandit import AdultDataBandit  # noqa
-from genrl.bandit.bandits.data_bandits.base import DataBasedBandit  # noqa
-from genrl.bandit.bandits.data_bandits.census_bandit import CensusDataBandit  # noqa
-from genrl.bandit.bandits.data_bandits.covertype_bandit import (
-    CovertypeDataBandit  # noqa,,,,,
-)
-from genrl.bandit.bandits.data_bandits.magic_bandit import MagicDataBandit  # noqa
-from genrl.bandit.bandits.data_bandits.mushroom_bandit import MushroomDataBandit  # noqa
-from genrl.bandit.bandits.data_bandits.statlog_bandit import StatlogDataBandit  # noqa
+from genrl.bandit.bandits.data_bandits.adult_bandit import AdultDataBandit
+from genrl.bandit.bandits.data_bandits.base import DataBasedBandit
+from genrl.bandit.bandits.data_bandits.census_bandit import CensusDataBandit
+from genrl.bandit.bandits.data_bandits.covertype_bandit import CovertypeDataBandit
+from genrl.bandit.bandits.data_bandits.magic_bandit import MagicDataBandit
+from genrl.bandit.bandits.data_bandits.mushroom_bandit import MushroomDataBandit
+from genrl.bandit.bandits.data_bandits.statlog_bandit import StatlogDataBandit
