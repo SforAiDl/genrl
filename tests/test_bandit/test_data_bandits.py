90ac9ee50cadd71fe2dc080ff28900d80bcc7f91
Atharv Sonwane <atharvs.twm@gmail.com>
2020-07-27 23:10:57 +0530

    restructrured test for data bandits

--- tests/test_bandit/test_data_bandits.py
+++ tests/test_bandit/test_data_bandits.py
@@ -9,73 +9,67 @@ from genrl.bandit import (
 
 
 class TestDataBandit:
-    def test_covertype_data_bandit_with_download(self) -> None:
+    def test_covertype_data_bandit(self) -> None:
         bandit = CovertypeDataBandit(download=True)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
             _, _ = bandit.step(i)
 
-    def test_mushroom_data_bandit_with_download(self) -> None:
-        bandit = MushroomDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_statlog_data_bandit_with_download(self) -> None:
-        bandit = StatlogDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_adult_data_bandit_with_download(self) -> None:
-        bandit = AdultDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_census_data_bandit_with_download(self) -> None:
-        bandit = CensusDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_magic_data_bandit_with_download(self) -> None:
-        bandit = MagicDataBandit(download=True)
-        _ = bandit.reset()
-        for i in range(bandit.n_actions):
-            _, _ = bandit.step(i)
-
-    def test_covertype_data_bandit_without_download(self) -> None:
         bandit = CovertypeDataBandit(download=False)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
             _, _ = bandit.step(i)
 
-    def test_mushroom_data_bandit_without_download(self) -> None:
+    def test_mushroom_data_bandit(self) -> None:
+        bandit = MushroomDataBandit(download=True)
+        _ = bandit.reset()
+        for i in range(bandit.n_actions):
+            _, _ = bandit.step(i)
+
         bandit = MushroomDataBandit(download=False)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
             _, _ = bandit.step(i)
 
-    def test_statlog_data_bandit_without_download(self) -> None:
+    def test_statlog_data_bandit(self) -> None:
+        bandit = StatlogDataBandit(download=True)
+        _ = bandit.reset()
+        for i in range(bandit.n_actions):
+            _, _ = bandit.step(i)
+
         bandit = StatlogDataBandit(download=False)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
             _, _ = bandit.step(i)
 
-    def test_adult_data_bandit_without_download(self) -> None:
+    def test_adult_data_bandit(self) -> None:
+        bandit = AdultDataBandit(download=True)
+        _ = bandit.reset()
+        for i in range(bandit.n_actions):
+            _, _ = bandit.step(i)
+
         bandit = AdultDataBandit(download=False)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
             _, _ = bandit.step(i)
 
-    def test_census_data_bandit_without_download(self) -> None:
+    def test_census_data_bandit(self) -> None:
+        bandit = CensusDataBandit(download=True)
+        _ = bandit.reset()
+        for i in range(bandit.n_actions):
+            _, _ = bandit.step(i)
+
         bandit = CensusDataBandit(download=False)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
             _, _ = bandit.step(i)
 
-    def test_magic_data_bandit_without_download(self) -> None:
+    def test_magic_data_bandit(self) -> None:
+        bandit = MagicDataBandit(download=True)
+        _ = bandit.reset()
+        for i in range(bandit.n_actions):
+            _, _ = bandit.step(i)
+
         bandit = MagicDataBandit(download=False)
         _ = bandit.reset()
         for i in range(bandit.n_actions):
