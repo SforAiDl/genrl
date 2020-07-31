9ce9716b18aab490e880c99cd530965db6bc4ba9
Darshan Kothari <64654390+Darshan-ko@users.noreply.github.com>
2020-07-31 21:55:02 +0530

    explanatory doc-a2c (#207)
    * explanatory doc-a2c
    
    * formated file
    
    * Inserted code for atari env and made other changes

--- genrl/deep/agents/a2c/a2c.py
+++ genrl/deep/agents/a2c/a2c.py
@@ -21,8 +21,8 @@ class A2C(OnPolicyAgent):
     :param env: The environment to learn from
     :param gamma: Discount factor
     :param actor_batch_size: Update batch size
-    :param lr_actor: Policy Network learning rate
-    :param lr_critic: Value Network learning rate
+    :param lr_policy: Policy Network learning rate
+    :param lr_value: Value Network learning rate
     :param num_episodes: Number of episodes
     :param timesteps_per_actorbatch: Number of timesteps per epoch
     :param max_ep_len: Maximum timesteps in an episode
@@ -39,8 +39,8 @@ class A2C(OnPolicyAgent):
     :type env: Gym Environment
     :type gamma: float
     :type actor_batch_size: int
-    :type lr_a: float
-    :type lr_c: float
+    :type lr_policy: float
+    :type lr_value: float
     :type num_episodes: int
     :type timesteps_per_actorbatch: int
     :type max_ep_len: int
