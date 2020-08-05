fe6a9ab312d05d4b271ad0362b148805f52a1563
mehulrastogi <mehul.rastogi.mehul@gmail.com>
2020-08-05 22:42:58 +0530

    modify example

--- examples/deep.py
+++ examples/deep.py
@@ -5,57 +5,7 @@ from genrl.deep.common import OffPolicyTrainer, OnPolicyTrainer
 from genrl.environments import VectorEnv
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Train Deep RL algorithms")
-    parser.add_argument(
-        "-a", "--algo", help="Which Algo to train", default="ppo", type=str
-    )
-    parser.add_argument(
-        "-e", "--env", help="Which env to train on", default="CartPole-v0", type=str
-    )
-    parser.add_argument(
-        "--env-type", help="What kind of env is it", default="gym", type=str
-    )
-    parser.add_argument(
-        "-n",
-        "--n-envs",
-        help="Number of vectorized envs to train on",
-        default=2,
-        type=int,
-    )
-    parser.add_argument(
-        "--serial",
-        help="Vectorized envs should be serial or parallel",
-        default=True,
-        type=bool,
-    )
-    parser.add_argument(
-        "--epochs", help="How many epochs to train on", default=20, type=int
-    )
-    parser.add_argument(
-        "--render",
-        help="Should the env be rendered",
-        default=False,
-        action="store_true",
-    )
-    parser.add_argument(
-        "--log", help="Comma separated string of logs", default="stdout", type=str
-    )
-    parser.add_argument(
-        "--arch", help="Which architecture mlp/cnn for now", default="mlp", type=str
-    )
-    parser.add_argument("--log-interval", help="Set Log interval", default=50, type=int)
-    parser.add_argument("--batch-size", help="Batch Size", default=128, type=int)
-
-    offpolicyargs = parser.add_argument_group("Off Policy Args")
-    offpolicyargs.add_argument(
-        "-ws", "--warmup-steps", help="Warmup steps", default=10000, type=int
-    )
-    offpolicyargs.add_argument(
-        "--replay-size", help="Replay Buffer Size", default=1000, type=int
-    )
-
-    args = parser.parse_args()
+def main(args):
 
     ALGOS = {
         "sac": SAC,
@@ -68,19 +18,14 @@ def main():
     }
 
     algo = ALGOS[args.algo.lower()]
-    env = VectorEnv(
-        args.env, n_envs=args.n_envs, parallel=not args.serial, env_type=args.env_type
-    )
+    env = VectorEnv(args.env, n_envs=args.n_envs, parallel=not args.serial, env_type=args.env_type)
 
     logger = get_logger(args.log)
+    trainer = None
 
-    offpolicy = True
-    trainerclass = OffPolicyTrainer
     if args.algo in ["ppo", "vpg", "a2c"]:
-        offpolicy = False
-        trainerclass = OnPolicyTrainer
         agent = algo(args.arch, env)  # , batch_size=args.batch_size)
-        trainer = trainerclass(
+        trainer = OnPolicyTrainer(
             agent,
             env,
             logger,
@@ -93,7 +38,7 @@ def main():
         agent = algo(
             args.arch, env, replay_size=args.replay_size, batch_size=args.batch_size
         )
-        trainer = trainerclass(
+        trainer = OffPolicyTrainer(
             agent,
             env,
             logger,
@@ -104,7 +49,8 @@ def main():
         )
 
     trainer.train()
-    trainer.evaluate()
+    trainer.evaluate(render = True)
+    env.render()
 
 
 def get_logger(log):
@@ -119,4 +65,23 @@ def get_logger(log):
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Train Deep RL algorithms")
+    parser.add_argument("-a", "--algo", help="Which Algo to train", default="ppo", type=str)
+    parser.add_argument("-e", "--env", help="Which env to train on", default="CartPole-v0", type=str)
+    parser.add_argument("--env-type", help="What kind of env is it", default="gym", type=str)
+    parser.add_argument("-n","--n-envs",help="Number of vectorized envs to train on",default=2,type=int)
+    parser.add_argument("--serial",help="Vectorized envs should be serial or parallel",default=True,type=bool)
+    parser.add_argument("--epochs", help="How many epochs to train on", default=20, type=int)
+    parser.add_argument("--render",help="Should the env be rendered",default=False,action="store_true")
+    parser.add_argument("--log", help="Comma separated string of logs", default="stdout", type=str)
+    parser.add_argument("--arch", help="Which architecture mlp/cnn for now", default="mlp", type=str)
+    parser.add_argument("--log-interval", help="Set Log interval", default=50, type=int)
+    parser.add_argument("--batch-size", help="Batch Size", default=128, type=int)
+
+    offpolicyargs = parser.add_argument_group("Off Policy Args")
+    offpolicyargs.add_argument("-ws", "--warmup-steps", help="Warmup steps", default=10000, type=int)
+    offpolicyargs.add_argument("--replay-size", help="Replay Buffer Size", default=1000, type=int)
+
+    args = parser.parse_args()
+
+    main(args)
