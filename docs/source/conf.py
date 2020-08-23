653390b0f7b7c77925610f6fb5010828c0e65f58
Atharv Sonwane <atharvs.twm@gmail.com>
2020-08-23 16:14:11 +0530

    added link to source code in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinxcontrib.napoleon",
     "sphinx_rtd_theme",
     "recommonmark",
+    "sphinx.ext.viewcode"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
