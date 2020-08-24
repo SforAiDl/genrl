4a3ace329eb631827f915c7c6fc7f24eb63d0867
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-08-25 04:10:20 +0530

    Restructure.

--- genrl/bandit/bandits/multi_armed_bandits/__init__.py
+++
@@ -1,3 +0,0 @@
-from genrl.bandit.bandits.multi_armed_bandits.base import MultiArmedBandit  # noqa
-from genrl.bandit.bandits.multi_armed_bandits.bernoulli_mab import BernoulliMAB  # noqa
-from genrl.bandit.bandits.multi_armed_bandits.gaussian_mab import GaussianMAB  # noqa
--- genrl/classical/common/__init__.py
+++
@@ -1,2 +0,0 @@
-from genrl.classical.common.models import TabularModel, get_model_from_name  # noqa
-from genrl.classical.common.trainer import Trainer  # noqa
