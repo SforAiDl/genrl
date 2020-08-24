733d6a6e689a0ed76902b41adb7382144b5fb76c
AdityaKapoor74 <adityakapoor074@gmail.com>
2020-08-25 04:49:41 +0530

    new changes

--- contrib/maa2c/example/maa2c.py
+++ contrib/maa2c/example/maa2c.py
@@ -1,23 +1,16 @@
 import sys
-sys.path.append("/home/aditya/Desktop/genrl/genrl") #add path
+sys.path.append("add path to /genrl/genrl") #add path
 import numpy as np
 
 from contrib.maa2c.a2c_agent import A2CAgent
 from simple_spread_test import make_env
-# from genrl.deep.common.trainer import OnPolicyTrainer
-# from genrl.environments import VectorEnv
 
 from utils import TensorboardLogger
-# from genrl.deep.common.logger import TensorboardLogger
 
 
 def train(max_episodes, timesteps_per_eps, logdir):
 
-	# CUSTOM
 	tensorboard_logger = TensorboardLogger(logdir)
-	# vec_env = VectorEnv(
- #        env_id="simple_spread", n_envs=1, parallel=False, env_type="multi"
- #        )
 
 	environment = make_env(scenario_name="simple_spread",benchmark=False)
 
@@ -43,13 +36,6 @@ def train(max_episodes, timesteps_per_eps, logdir):
 
 
 	tensorboard_logger.close()
-
-	# on_policy_trainer = OnPolicyTrainer(agent=a2c_agent,env=vec_env, log_mode=["tensorboard"], log_key="Episode" , save_interval=100, save_model="checkpoints", steps_per_epoch=timesteps_per_eps, epochs=max_episodes,
-	# 	log_interval=1, batch_size=1)
-
-	# on_policy_trainer.train()
-
-
 
 
 
