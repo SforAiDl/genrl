2c2bf47481af490c91651541d76f07a786547bcd
Aditya Kapoor <adityakapoor074@gmail.com>
2020-09-01 19:22:24 +0530

    SharedActorCritic

---
+++ contrib/models/actor_critic.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+import torch.autograd as autograd
+from typing import Tuple, Type, Union
+import numpy as np
+
+import sys
+sys.path.append("/home/aditya/Desktop/genrl/genrl")
+from contrib.utils.models import SharedMLP
+
+
+class BaseActorCritic(nn.Module):
+    """
+    Basic implementation of a general Actor Critic
+    """
+
+    def __init__(self):
+        super(BaseActorCritic, self).__init__()
+
+        self.actor = None
+        self.critic = None
+        self.actorcritic = None
+
+    def get_action(
+        self, state: torch.Tensor, deterministic: bool = False
+    ) -> torch.Tensor:
+        """
+                Get action from the Actor based on input
+
+                :param state: The state being passed as input to the Actor
+                :param deterministic: (True if the action space is deterministic,
+        else False)
+                :type state: Tensor
+                :type deterministic: boolean
+                :returns: action
+        """
+        state = torch.as_tensor(state).float()
+        if self.actor == None:
+            return self.actorcritic.get_action(None,state,deterministic=deterministic)
+        return self.actor.get_action(state, deterministic=deterministic)
+
+    def get_value(self, state: torch.Tensor) -> torch.Tensor:
+        """
+        Get value from the Critic based on input
+
+        :param state: Input to the Critic
+        :type state: Tensor
+        :returns: value
+        """
+        state = torch.as_tensor(state).float()
+        if self.critic == None:
+            return self.actorcritic.get_value(state,None)
+        return self.critic.get_value(state)
+
+
+
+class SharedActorCritic(BaseActorCritic):
+    def __init__(self, critic_prev,actor_prev,shared,critic_post,actor_post,weight_init,activation_func):
+        super(SharedActorCritic, self).__init__()
+
+        self.actorcritic = SharedMLP(critic_prev,actor_prev,shared,critic_post,actor_post,weight_init,activation_func)
+        print(self.actorcritic)
+
+    def forward(self, state_critic,state_action):
+
+        if state_critic is not None:
+
+            for i in range(len(self.actorcritic.network1_prev)):
+                if self.actorcritic.activation is not None:
+                    state_critic = self.actorcritic.activation(self.actorcritic.network1_prev[i](state_critic))
+                else:
+                    state_critic = self.actorcritic.network1_prev[i](state_critic)
+
+            for i in range(len(self.actorcritic.shared)):
+                if self.actorcritic.activation is not None:
+                    state_critic = self.actorcritic.activation(self.actorcritic.shared[i](state_critic))
+                else:
+                    state_critic = self.actorcritic.shared[i](state_critic)
+
+            for i in range(len(self.actorcritic.network1_post)):
+                if self.actorcritic.activation is not None:
+                    state_critic = self.actorcritic.activation(self.actorcritic.network1_post[i](state_critic))
+                else:
+                    state_critic = self.actorcritic.network1_post[i](state_critic)
+
+            return state_critic
+
+        if state_action is not None:
+
+            for i in range(len(self.actorcritic.network2_prev)):
+                if self.actorcritic.activation is not None:
+                    state_action = self.actorcritic.activation(self.actorcritic.network2_prev[i](state_action))
+                else:
+                    state_action = self.actorcritic.network2_prev[i](state_action)
+
+            for i in range(len(self.actorcritic.shared)):
+                if self.actorcritic.activation is not None:
+                    state_action = self.actorcritic.activation(self.actorcritic.shared[i](state_action))
+                else:
+                    state_action = self.actorcritic.shared[i](state_action)
+
+            for i in range(len(self.actorcritic.network2_post)):
+                if self.actorcritic.activations is not None:
+                    state_action = self.actorcritic.activation(self.actorcritic.network2_post[i](state_action))
+                else:
+                    state_action = self.actorcritic.network2_post[i](state_action)
+
+            return state_action
+
+
+
+    def get_action(self, state, device, one_hot=False):
+        state = torch.FloatTensor(state).to(device)
+        logits = self.forward(None,state)
+        if one_hot:
+            logits = self.onehot_from_logits(logits)
+            return logits
+
+        dist = F.softmax(logits, dim=0)
+        probs = Categorical(dist)
+        index = probs.sample().cpu().detach().item()
+        return index
+
+    def onehot_from_logits(self, logits, eps=0.0):
+        # get best (according to current policy) actions in one-hot form
+        argmax_acs = (logits == logits.max(0, keepdim=True)[0]).float()
+        if eps == 0.0:
+            return argmax_acs
+        # get random actions in one-hot form
+        rand_acs = torch.eye(logits.shape[1])[
+            [np.random.choice(range(logits.shape[1]), size=logits.shape[0])]
+        ]
+        # chooses between best and random actions using epsilon greedy
+        return torch.stack(
+            [
+                argmax_acs[i] if r > eps else rand_acs[i]
+                for i, r in enumerate(torch.rand(logits.shape[0]))
+            ]
+        )
+
+
+
+
+model = SharedActorCritic([1,1],[1,1],[1,1],[1,1],[1,1],"xavier_uniform","relu")
+forward = model.forward(torch.Tensor([1]),None)
+print(forward)
