e93cdcc2527b94f3815c25aa0fe9e11a0903822b
Sampreet <sampreetarthi@gmail.com>
2020-09-01 20:14:28 +0530

    Off Policy Trainer refactoring (#287)
    * minor error fixes
    
    * Made dones simpler and made methods to make code looks simpler.
    
    * remove testing file
    
    * lint errors
    
    * minor changes
    
    * unused import
    
    * Add requirements to Lint phase of Actions workflow
    
    * pre-commit fix
    
    * linting fix
    
    * correct long test fix
    
    * correct long test fix
    
    * shortened tests time

--- genrl/environments/vec_env/vector_envs.py
+++ genrl/environments/vec_env/vector_envs.py
@@ -1,4 +1,3 @@
-import copy
 import multiprocessing as mp
 from abc import ABC, abstractmethod
 from typing import Any, Iterator, List, Tuple
@@ -158,19 +157,16 @@ class SerialVecEnv(VecEnv):
         """
         for i, env in enumerate(self.envs):
             obs, reward, done, info = env.step(actions[i])
-            if done:
-                obs = env.reset()
             self.states[i] = obs
             self.episode_reward[i] += reward
             self.rewards[i] = reward
             self.dones[i] = done
             self.infos[i] = info
-
         return (
             np.copy(self.states),
             self.rewards.copy(),
             self.dones.copy(),
-            copy.deepcopy(self.infos),
+            self.infos,
         )
 
     def reset(self) -> np.ndarray:
@@ -210,11 +206,11 @@ class SerialVecEnv(VecEnv):
 
     def render(self, mode="human"):
         """
-                Renders all envs in a tiles format similar to baselines
+        Renders all envs in a tiles format similar to baselines
 
-                :param mode: (Can either be 'human' or 'rgb_array'. Displays tiled
-        images in 'human' and returns tiled images in 'rgb_array')
-                :type mode: string
+        :param mode: (Can either be 'human' or 'rgb_array'. Displays tiled
+            images in 'human' and returns tiled images in 'rgb_array')
+        :type mode: string
         """
         self.env.render()
 
