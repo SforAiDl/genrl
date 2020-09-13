478543171a79dda6a3885849a9f4b6fbe8c5c4a2
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-09-13 15:05:54 +0530

    shebang directive

--- .scripts/windows_cpu_build.ps1
+++ .scripts/windows_cpu_build.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env pwsh
+
 conda install -c conda-forge swig
 python -m pip install --upgrade pip
 pip install torch==1.4.0 --quiet --find-links https://download.pytorch.org/whl/cpu/torch_stable.html --upgrade
