83ff1704c7daf967cb6a02c716bbcaee89a019f9
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-09-13 13:30:35 +0530

    expand-archive command

--- .scripts/windows_build.ps1
+++ .scripts/windows_build.ps1
@@ -8,9 +8,13 @@ function Unzip
     [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
-Invoke-WebRequest -uri http://prdownloads.sourceforge.net/swig/swigwin-4.0.2.zip -Method "GET" -OutFile "D:\a\swigwin-4.0.2.zip"
-Unzip "D:\a\swigwin-4.0.2.zip" "D:\a\swigwin-4.0.2"
-copy-item -path "D:\a\swigwin-4.0.2\swig.exe" -destination ".\"
+# Invoke-WebRequest -uri http://prdownloads.sourceforge.net/swig/swigwin-4.0.2.zip -Method "GET" -OutFile "D:\a\swigwin-4.0.2.zip"
+# Unzip "D:\a\swigwin-4.0.2.zip" "D:\a\swigwin-4.0.2"
+# copy-item -path "D:\a\swigwin-4.0.2\swig.exe" -destination ".\"
+
+(New-Object System.Net.WebClient).DownloadFile("http://prdownloads.sourceforge.net/swig/swigwin-4.0.1.zip","swigwin-4.0.1.zip");
+Expand-Archive .\swigwin-4.0.1.zip .;
+Copy-item -path ".\swigwin-4.0.2\swig.exe" -destination ".\"
 
 pip install torch==1.4.0 --quiet --find-links https://download.pytorch.org/whl/cpu/torch_stable.html --upgrade
 pip install -r requirements.txt
