df91f060fde46bd17899db6c14917cee54e68f3b
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-09-13 13:18:09 +0530

    Extract into specified directory

--- .scripts/windows_build.ps1
+++ .scripts/windows_build.ps1
@@ -9,7 +9,7 @@ function Unzip
 }
 
 Invoke-WebRequest -uri http://prdownloads.sourceforge.net/swig/swigwin-4.0.2.zip -Method "GET" -OutFile swigwin-4.0.2.zip
-Unzip "swigwin-4.0.2.zip" ".\"
+Unzip "swigwin-4.0.2.zip" ".\swigwin-4.0.2"
 copy-item -path "swigwin-4.0.2\swig.exe" -destination ".\"
 
 pip install torch==1.4.0 --quiet --find-links https://download.pytorch.org/whl/cpu/torch_stable.html --upgrade
