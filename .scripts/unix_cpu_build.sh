da535e2f42d1d110c89bbe8e6feea791cc505f0a
Rishabh Patra <rpatra.rp54@gmail.com>
2020-09-17 22:07:27 +0530

    Modify codecov and build.sh

--- .scripts/unix_cpu_build.sh
+++ .scripts/unix_cpu_build.sh
@@ -3,3 +3,4 @@
 python -m pip install --upgrade pip
 pip install torch==1.4.0 --quiet  --find-links https://download.pytorch.org/whl/cpu/torch_stable.html --upgrade
 pip install -r requirements.txt
+pip install git+https://github.com/eleurent/highway-env
--- .scripts/windows_cpu_build.ps1
+++ .scripts/windows_cpu_build.ps1
@@ -4,3 +4,4 @@ conda install -c conda-forge swig
 python -m pip install --upgrade pip
 pip install torch==1.4.0 --quiet --find-links https://download.pytorch.org/whl/cpu/torch_stable.html --upgrade
 pip install -r requirements.txt
+pip install git+https://github.com/eleurent/highway-env
