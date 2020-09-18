f240cc5055286fb9e5388c60bcd2ae930b2f3dac
Sharad24 <f20170472@goa.bits-pilani.ac.in>
2020-09-18 12:40:05 +0530

    Windows fix (1)

--- .scripts/windows_cpu_build.ps1
+++ .scripts/windows_cpu_build.ps1
@@ -1,6 +1,5 @@
 #!/usr/bin/env pwsh
 
-pip install -f https://github.com/Kojoley/atari-py/releases atari_py
 conda install -c conda-forge swig
 python -m pip install --upgrade pip
 pip install torch==1.4.0 --quiet --find-links https://download.pytorch.org/whl/cpu/torch_stable.html --upgrade
