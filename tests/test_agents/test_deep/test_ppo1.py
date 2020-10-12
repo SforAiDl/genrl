25eb018f18a9a1d0865c16e5233a2a7ccddbfd78
Sampreet <sampreetarthi@gmail.com>
2020-10-12 12:18:07 +0530

    Fixed couple of errors and added new tests (#380)
    * minor error fixes
    
    * Fixed Evaluate error and Action/Obs shape errors
    
    * code climate fix
    
    * Modularised tests and added few new ones

--- tests/test_agents/test_deep/test_ppo1.py
+++ tests/test_agents/test_deep/test_ppo1.py
@@ -5,25 +5,39 @@ from genrl.environments import VectorEnv
 from genrl.trainers import OnPolicyTrainer
 
 
-def test_ppo1():
-    env = VectorEnv("CartPole-v0")
-    algo = PPO1("mlp", env, rollout_size=128)
-    trainer = OnPolicyTrainer(algo, env, log_mode=["csv"], logdir="./logs", epochs=1)
-    trainer.train()
-    shutil.rmtree("./logs")
+class TestPPO:
+    def test_ppo1(self):
+        env = VectorEnv("CartPole-v0")
+        algo = PPO1("mlp", env, rollout_size=128)
+        trainer = OnPolicyTrainer(
+            algo, env, log_mode=["csv"], logdir="./logs", epochs=1
+        )
+        trainer.train()
+        shutil.rmtree("./logs")
 
+    def test_ppo1_continuous(self):
+        env = VectorEnv("Pendulum-v0")
+        algo = PPO1("mlp", env, rollout_size=128)
+        trainer = OnPolicyTrainer(
+            algo, env, log_mode=["csv"], logdir="./logs", epochs=1
+        )
+        trainer.train()
+        shutil.rmtree("./logs")
 
-def test_ppo1_cnn():
-    env = VectorEnv("Pong-v0", env_type="atari")
-    algo = PPO1("cnn", env, rollout_size=128)
-    trainer = OnPolicyTrainer(algo, env, log_mode=["csv"], logdir="./logs", epochs=1)
-    trainer.train()
-    shutil.rmtree("./logs")
+    def test_ppo1_cnn(self):
+        env = VectorEnv("Pong-v0", env_type="atari")
+        algo = PPO1("cnn", env, rollout_size=128)
+        trainer = OnPolicyTrainer(
+            algo, env, log_mode=["csv"], logdir="./logs", epochs=1
+        )
+        trainer.train()
+        shutil.rmtree("./logs")
 
-
-def test_ppo1_shared():
-    env = VectorEnv("CartPole-v0")
-    algo = PPO1("mlp", env, shared_layers=[32, 32], rollout_size=128)
-    trainer = OnPolicyTrainer(algo, env, log_mode=["csv"], logdir="./logs", epochs=1)
-    trainer.train()
-    shutil.rmtree("./logs")
+    def test_ppo1_shared(self):
+        env = VectorEnv("CartPole-v0")
+        algo = PPO1("mlp", env, shared_layers=[32, 32], rollout_size=128)
+        trainer = OnPolicyTrainer(
+            algo, env, log_mode=["csv"], logdir="./logs", epochs=1
+        )
+        trainer.train()
+        shutil.rmtree("./logs")
--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -1,3 +1,3 @@
-from tests.test_utils.test_logger import test_loggers
-from tests.test_utils.test_models import test_tabular_model
+from tests.test_utils.test_logger import TestLogger
+from tests.test_utils.test_models import TestClassicalModel
 from tests.test_utils.test_utils import TestUtils
