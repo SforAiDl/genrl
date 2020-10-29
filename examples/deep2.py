59e960cedff64d31406cef7346685221be242bb3
Atharv Sonwane <atharvs.twm@gmail.com>
2020-10-29 10:43:22 +0000

    removed temp example

--- examples/deep2.py
+++
@@ -1,9 +0,0 @@
-from genrl.agents import DDPG
-from genrl.environments import VectorEnv
-from genrl.trainers import OffPolicyTrainer
-
-env = VectorEnv("Pendulum-v0", n_envs=1)
-agent = DDPG("mlp", env)
-trainer = OffPolicyTrainer(agent, env, max_timesteps=20000)
-trainer.train()
-trainer.evaluate()
